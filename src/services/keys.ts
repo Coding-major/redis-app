e5677707714d7109b1fdc61149c24853a7cf360d
Coding-major <shehumustapham@gmail.com>
2024-04-23 08:30:17 +0100

    checking if the return session object is null

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -1,2 +1,3 @@
 export const pagecacheKey = (id: string) => `pagecache#${id}`
 export const usersKey = (userId: string) => `users#${userId}`
+export const sessionKey = (sessionId: string) => `session#${sessionId}`
--- src/services/queries/sessions.ts
+++ src/services/queries/sessions.ts
@@ -1,5 +1,34 @@
 import type { Session } from '$services/types';
+import { sessionKey } from '$services/keys';
+import { genId } from '$services/utils';
+import { client } from '$services/redis';
 
-export const getSession = async (id: string) => {};
 
-export const saveSession = async (session: Session) => {};
+export const getSession = async (id: string) => {
+    const session = await client.hGetAll(sessionKey(id))
+
+    // if (Object.entries(session).length > 0) {
+    //     return session
+    // } else {
+    //     return null
+    // }
+
+    if (Object.keys(session).length === 0) {
+        return null
+    }
+
+    return deserialize(id, session)
+};
+
+export const saveSession = async (session: Session) => {
+    // const id = genId()
+    // await client.hSet()
+};
+
+const deserialize = (id: string, session: {[key: string]: string}) => {
+    return {
+        id,
+        userId: session.userId,
+        username: session.username
+    }
+}
